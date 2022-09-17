0a73f7965e3d3bd95844b5170378bd6adfa255c5
Kota <maedakoutaa@gmail.com>
2022-09-17 15:23:23 +0900

    Initial Commit

---
+++ MiniApp97-AppStorage-UserDefault/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MiniApp97-AppStorage-UserDefault
+//
+//  Created by 前田航汰 on 2022/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp97-AppStorage-UserDefault/MiniApp97_AppStorage_UserDefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp97_AppStorage_UserDefaultApp.swift
+//  MiniApp97-AppStorage-UserDefault
+//
+//  Created by 前田航汰 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp97_AppStorage_UserDefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
